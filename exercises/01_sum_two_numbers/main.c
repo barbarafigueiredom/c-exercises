6f681f25d20673ff92874ba62598b096329e7cb6
Barbara Figueiredo <barbarafigueiredo74@gmail.com>
2026-04-18 02:25:30 -0300

    exercise 1

--- exercises/01_sum_two_numbers/main.c
+++ exercises/01_sum_two_numbers/main.c
@@ -15,10 +15,9 @@ void print_test_result(int index, const char *input, const char *expected, const
 }
 
 int solve(int a, int b) {
-    // TODO: implemente a logica deste exercicio.
-    (void)a;
-    (void)b;
-    return 0;
+    int soma = a + b;
+
+    return soma;
 }
 
 void run_tests(void) {
