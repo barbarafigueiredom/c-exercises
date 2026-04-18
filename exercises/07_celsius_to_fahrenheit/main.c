e0d1798bc9e4d543b30745ed4bb3e8557c0addab
Barbara Figueiredo <barbarafigueiredo74@gmail.com>
2026-04-18 04:10:29 -0300

    exercise 7

--- exercises/07_celsius_to_fahrenheit/main.c
+++ exercises/07_celsius_to_fahrenheit/main.c
@@ -15,9 +15,8 @@ void print_test_result(int index, const char *input, const char *expected, const
 }
 
 double solve(double value) {
-    // TODO: implemente a logica deste exercicio.
-    (void)value;
-    return 0.0;
+    double f = (value * 9 / 5) + 32;
+    return f;
 }
 
 void run_tests(void) {
