5cb014e5098c89398b3a817f4f0bbe6d5e7ebef0
Barbara Figueiredo <barbarafigueiredo74@gmail.com>
2026-04-18 02:36:45 -0300

    exercise 2

--- exercises/02_subtract_two_numbers/main.c
+++ exercises/02_subtract_two_numbers/main.c
@@ -15,10 +15,9 @@ void print_test_result(int index, const char *input, const char *expected, const
 }
 
 int solve(int a, int b) {
-    // TODO: implemente a logica deste exercicio.
-    (void)a;
-    (void)b;
-    return 0;
+    int sub = a - b;
+    
+    return sub;
 }
 
 void run_tests(void) {
